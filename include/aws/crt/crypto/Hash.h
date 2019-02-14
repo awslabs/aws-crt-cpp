a93f13368984ea832f122effc63f9e6df036ed6a
Jonathan M. Henson <henso@amazon.com>
2019-02-13 19:48:57 -0800

    Added aws-c-cal wrapper and basic binding correctness verification.

---
+++ include/aws/crt/crypto/Hash.h
@@ -0,0 +1,123 @@
+#pragma once
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+#include <aws/crt/Exports.h>
+#include <aws/crt/Types.h>
+
+struct aws_hash;
+namespace Aws
+{
+    namespace Crt
+    {
+        namespace Crypto
+        {
+            static const size_t SHA256_DIGEST_SIZE = 32;
+            static const size_t MD5_DIGEST_SIZE = 16;
+
+            /**
+             * Computes a SHA256 Hash over input, and writes the digest to output. If truncateTo is non-zero, the digest
+             * will be truncated to the value of truncateTo. Returns true on success. If this function fails,
+             * Aws::Crt::LastError() will contain the error that occured. Unless you're using 'truncateTo', output
+             * should have a minimum capacity of SHA256_DIGEST_SIZE.
+             */
+            bool AWS_CRT_CPP_API ComputeSHA256(
+                Allocator *allocator,
+                const ByteCursor &input,
+                ByteBuf &output,
+                size_t truncateTo = 0) noexcept;
+
+            /**
+             * Computes a SHA256 Hash using the default allocator over input, and writes the digest to output. If
+             * truncateTo is non-zero, the digest will be truncated to the value of truncateTo. Returns true on success.
+             * If this function fails, Aws::Crt::LastError() will contain the error that occured. Unless you're using
+             * 'truncateTo', output should have a minimum capacity of SHA256_DIGEST_SIZE.
+             */
+            bool AWS_CRT_CPP_API
+                ComputeSHA256(const ByteCursor &input, ByteBuf &output, size_t truncateTo = 0) noexcept;
+
+            /**
+             * Computes a MD5 Hash over input, and writes the digest to output. If truncateTo is non-zero, the digest
+             * will be truncated to the value of truncateTo. Returns true on success. If this function fails,
+             * Aws::Crt::LastError() will contain the error that occured. Unless you're using 'truncateTo',
+             * output should have a minimum capacity of MD5_DIGEST_SIZE.
+             */
+            bool AWS_CRT_CPP_API ComputeMD5(
+                Allocator *allocator,
+                const ByteCursor &input,
+                ByteBuf &output,
+                size_t truncateTo = 0) noexcept;
+
+            /**
+             * Computes a MD5 Hash using the default allocator over input, and writes the digest to output. If
+             * truncateTo is non-zero, the digest will be truncated to the value of truncateTo. Returns true on success.
+             * If this function fails, Aws::Crt::LastError() will contain the error that occured. Unless you're using
+             * 'truncateTo', output should have a minimum capacity of MD5_DIGEST_SIZE.
+             */
+            bool AWS_CRT_CPP_API ComputeMD5(const ByteCursor &input, ByteBuf &output, size_t truncateTo = 0) noexcept;
+
+            /**
+             * Streaming Hash object. The typical use case is for computing the hash of an object that is too large to
+             * load into memory. You can call Update() mutliple times as you load chunks of data into memory. When
+             * you're finished simply call Digest(). After Digest() is called, this object is no longer usable.
+             */
+            class AWS_CRT_CPP_API Hash final
+            {
+              public:
+                ~Hash();
+                Hash(const Hash &) = delete;
+                Hash &operator=(const Hash &) = delete;
+                Hash(Hash &&toMove);
+                Hash &operator=(Hash &&toMove);
+
+                inline operator bool() const noexcept { return m_good; }
+
+                inline int LastError() const noexcept { return m_lastError; }
+
+                /**
+                 * Creates an instance of a Streaming SHA256 Hash.
+                 */
+                static Hash CreateSHA256(Allocator *allocator = DefaultAllocator()) noexcept;
+
+                /**
+                 * Creates an instance of a Streaming MD5 Hash.
+                 */
+                static Hash CreateMD5(Allocator *allocator = DefaultAllocator()) noexcept;
+
+                /**
+                 * Updates the running hash object with data in toHash. Returns true on success. Call
+                 * LastError() for the reason this call failed.
+                 */
+                bool Update(const ByteCursor &toHash) noexcept;
+
+                /**
+                 * Finishes the running hash operation and writes the digest into output. The available capacity of
+                 * output must be large enough for the digest. See: SHA256_DIGEST_SIZE and MD5_DIGEST_SIZE for size
+                 * hints. 'truncateTo' is for if you want truncated output (e.g. you only want the first 16 bytes of a
+                 * SHA256 digest. Returns true on success. Call LastError() for the reason this call failed.
+                 */
+                bool Digest(ByteBuf &output, size_t truncateTo = 0) noexcept;
+
+              private:
+                Hash(aws_hash *hash) noexcept;
+                Hash() = delete;
+
+                aws_hash *m_hash;
+                bool m_good;
+                int m_lastError;
+            };
+
+        } // namespace Crypto
+    }     // namespace Crt
+} // namespace Aws
---
+++ tests/HashTest.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+#include <aws/crt/Api.h>
+#include <aws/crt/crypto/Hash.h>
+#include <aws/testing/aws_test_harness.h>
+
+static int s_TestSHA256ResourceSafety(struct aws_allocator *allocator, void *)
+{
+    Aws::Crt::ApiHandle apiHandle(allocator);
+    Aws::Crt::Crypto::Hash sha256 = Aws::Crt::Crypto::Hash::CreateSHA256(allocator);
+    ASSERT_TRUE(sha256);
+
+    Aws::Crt::ByteCursor input = aws_byte_cursor_from_c_str("abc");
+    uint8_t expected[] = {
+        0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea, 0x41, 0x41, 0x40, 0xde, 0x5d, 0xae, 0x22, 0x23,
+        0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17, 0x7a, 0x9c, 0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad,
+    };
+    Aws::Crt::ByteBuf expectedBuf = Aws::Crt::ByteBufFromArray(expected, sizeof(expected));
+
+    uint8_t output[Aws::Crt::Crypto::SHA256_DIGEST_SIZE] = {0};
+    Aws::Crt::ByteBuf outputBuf = Aws::Crt::ByteBufFromArray(output, sizeof(output));
+    outputBuf.len = 0;
+
+    ASSERT_TRUE(sha256.Update(input));
+    ASSERT_TRUE(sha256.Digest(outputBuf));
+    ASSERT_FALSE(sha256);
+
+    ASSERT_BIN_ARRAYS_EQUALS(expectedBuf.buffer, expectedBuf.len, outputBuf.buffer, outputBuf.len);
+
+    return AWS_OP_SUCCESS;
+}
+
+AWS_TEST_CASE(SHA256ResourceSafety, s_TestSHA256ResourceSafety)
+
+static int s_TestMD5ResourceSafety(struct aws_allocator *allocator, void *)
+{
+    Aws::Crt::ApiHandle apiHandle(allocator);
+    Aws::Crt::Crypto::Hash md5 = Aws::Crt::Crypto::Hash::CreateMD5(allocator);
+    ASSERT_TRUE(md5);
+
+    Aws::Crt::ByteCursor input = aws_byte_cursor_from_c_str("abc");
+    uint8_t expected[] = {
+        0x90,
+        0x01,
+        0x50,
+        0x98,
+        0x3c,
+        0xd2,
+        0x4f,
+        0xb0,
+        0xd6,
+        0x96,
+        0x3f,
+        0x7d,
+        0x28,
+        0xe1,
+        0x7f,
+        0x72,
+    };
+    Aws::Crt::ByteBuf expectedBuf = Aws::Crt::ByteBufFromArray(expected, sizeof(expected));
+
+    uint8_t output[Aws::Crt::Crypto::MD5_DIGEST_SIZE] = {0};
+    Aws::Crt::ByteBuf outputBuf = Aws::Crt::ByteBufFromArray(output, sizeof(output));
+    outputBuf.len = 0;
+
+    ASSERT_TRUE(md5.Update(input));
+    ASSERT_TRUE(md5.Digest(outputBuf));
+    ASSERT_FALSE(md5);
+
+    ASSERT_BIN_ARRAYS_EQUALS(expectedBuf.buffer, expectedBuf.len, outputBuf.buffer, outputBuf.len);
+
+    return AWS_OP_SUCCESS;
+}
+
+AWS_TEST_CASE(MD5ResourceSafety, s_TestMD5ResourceSafety)
