310a7555555dd305147d96945260c76d861880b0
Justin Boswell <boswej@amazon.com>
2019-03-26 16:35:55 -0700

    Current state of IoT service test

--- tests/IotServiceTest.cpp
+++ tests/IotServiceTest.cpp
@@ -20,6 +20,8 @@
 #include <condition_variable>
 #include <mutex>
 
+#include <aws/io/logging.h>
+
 static int s_TestIotPublishSubscribe(Aws::Crt::Allocator *allocator, void *ctx)
 {
     using namespace Aws::Crt;
@@ -28,6 +30,15 @@ static int s_TestIotPublishSubscribe(Aws::Crt::Allocator *allocator, void *ctx)
 
     (void)ctx;
     Aws::Crt::ApiHandle apiHandle(allocator);
+
+//    aws_logger logger;
+//    aws_logger_standard_options log_options;
+//    log_options.level = AWS_LL_TRACE;
+//    log_options.file = stdout;
+//
+//    aws_logger_init_standard(&logger, allocator, &log_options);
+//    aws_logger_set(&logger);
+
     Aws::Crt::Io::TlsContextOptions tlsCtxOptions =
             Aws::Crt::Io::TlsContextOptions::InitClientWithMtls("/tmp/certificate.pem", "/tmp/privatekey.pem");
     tlsCtxOptions.OverrideDefaultTrustStore(nullptr, "/tmp/AmazonRootCA1.pem");
@@ -49,78 +60,84 @@ static int s_TestIotPublishSubscribe(Aws::Crt::Allocator *allocator, void *ctx)
     Aws::Crt::Mqtt::MqttClient mqttClient(clientBootstrap, allocator);
     ASSERT_TRUE(mqttClient);
 
-    auto mqttConnection =
-        mqttClient.NewConnection("a16523t7iy5uyg-ats.iot.us-east-1.amazonaws.com", 8883, socketOptions, tlsContext.NewConnectionOptions());
+    int tries = 0;
+    while (tries++ < 1000) {
+        auto mqttConnection =
+                mqttClient.NewConnection("a16523t7iy5uyg-ats.iot.us-east-1.amazonaws.com", 8883, socketOptions,
+                                         tlsContext.NewConnectionOptions());
 
-    std::mutex mutex;
-    std::condition_variable cv;
-    bool connected = false;
-    bool subscribed = false;
-    bool published = false;
-    bool received = false;
-    auto onConnectionCompleted = [&](MqttConnection &connection, int errorCode, ReturnCode returnCode, bool sessionPresent) {
-        printf("CONNECTED\n");
-        connected = true;
-        cv.notify_one();
-    };
-    auto onDisconnect = [&](MqttConnection& connection) {
-        printf("DISCONNECTED\n");
-        connected = false;
-        cv.notify_one();
-    };
-    auto onTest = [&](MqttConnection& connection, const String& topic, const ByteBuf& payload) {
-        printf("GOT MESSAGE @ %s\n", topic.c_str());
-        received = true;
-        cv.notify_one();
-    };
-    auto onSubAck = [&](MqttConnection& connection, uint16_t packetId, const String &topic, QOS qos, int errorCode) {
-        printf("SUBACK @ %s qos: %d\n", topic.c_str(), qos);
-        subscribed = true;
-        cv.notify_one();
-    };
-    auto onPubAck = [&](MqttConnection& connection, uint16_t packetId, int errorCode) {
-        printf("PUBLISHED @ %d\n", packetId);
-        published = true;
-        cv.notify_one();
-    };
+        std::mutex mutex;
+        std::condition_variable cv;
+        bool connected = false;
+        bool subscribed = false;
+        bool published = false;
+        bool received = false;
+        auto onConnectionCompleted = [&](MqttConnection &connection, int errorCode, ReturnCode returnCode,
+                                         bool sessionPresent) {
+            printf("CONNECTED\n");
+            connected = true;
+            cv.notify_one();
+        };
+        auto onDisconnect = [&](MqttConnection &connection) {
+            printf("DISCONNECTED\n");
+            connected = false;
+            cv.notify_one();
+        };
+        auto onTest = [&](MqttConnection &connection, const String &topic, const ByteBuf &payload) {
+            printf("GOT MESSAGE topic=%s payload=" PRInSTR "\n", topic.c_str(), AWS_BYTE_BUF_PRI(payload));
+            received = true;
+            cv.notify_one();
+        };
+        auto onSubAck = [&](MqttConnection &connection, uint16_t packetId, const String &topic, QOS qos,
+                            int errorCode) {
+            printf("SUBACK id=%d topic=%s qos=%d\n", packetId, topic.c_str(), qos);
+            subscribed = true;
+            cv.notify_one();
+        };
+        auto onPubAck = [&](MqttConnection &connection, uint16_t packetId, int errorCode) {
+            printf("PUBLISHED id=%d\n", packetId);
+            published = true;
+            cv.notify_one();
+        };
 
-    mqttConnection->OnConnectionCompleted = onConnectionCompleted;
-    mqttConnection->OnDisconnect = onDisconnect;
-    mqttConnection->Connect("aws-crt-cpp-v2", true);
+        mqttConnection->OnConnectionCompleted = onConnectionCompleted;
+        mqttConnection->OnDisconnect = onDisconnect;
+        mqttConnection->Connect("aws-crt-cpp-v2", true);
 
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        cv.wait(lock, [&]() { return connected; });
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [&]() { return connected; });
+        }
+
+        mqttConnection->Subscribe("/test/me/senpai", QOS::AWS_MQTT_QOS_AT_LEAST_ONCE, onTest, onSubAck);
+
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [&]() { return subscribed; });
+        }
+
+        Aws::Crt::ByteBuf payload = Aws::Crt::ByteBufFromCString("notice me pls");
+        mqttConnection->Publish("/test/me/senpai", QOS::AWS_MQTT_QOS_AT_LEAST_ONCE, false, payload, onPubAck);
+
+        // wait for publish
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [&]() { return published; });
+        }
+
+        // wait for message received callback
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [&]() { return received; });
+        }
+
+        mqttConnection->Disconnect();
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [&]() { return !connected; });
+        }
+        ASSERT_TRUE(mqttConnection);
     }
-
-//    mqttConnection->Subscribe("/test/me/senpai", QOS::AWS_MQTT_QOS_AT_LEAST_ONCE, onTest, onSubAck);
-//
-//    {
-//        std::unique_lock<std::mutex> lock(mutex);
-//        cv.wait(lock, [&]() { return subscribed; });
-//    }
-//
-//    Aws::Crt::ByteBuf payload = Aws::Crt::ByteBufFromCString("TEST");
-//    mqttConnection->Publish("/test/me/senpai", QOS::AWS_MQTT_QOS_AT_LEAST_ONCE, false, payload, onPubAck);
-//
-//    // wait for publish
-//    {
-//        std::unique_lock<std::mutex> lock(mutex);
-//        cv.wait(lock, [&]() { return published; });
-//    }
-//
-//    // wait for message received callback
-//    {
-//        std::unique_lock<std::mutex> lock(mutex);
-//        cv.wait(lock, [&]() { return received; });
-//    }
-
-    mqttConnection->Disconnect();
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        cv.wait(lock, [&]() { return !connected; });
-    }
-    ASSERT_TRUE(mqttConnection);
 
     return AWS_ERROR_SUCCESS;
 }
