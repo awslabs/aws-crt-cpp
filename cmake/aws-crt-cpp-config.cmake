39dada60244f6e3c0a0955b0de62f5b81bc35b7d
Bret Ambrose <bretambrose@gmail.com>
2019-07-10 12:22:03 -0700

    Integrate aws-c-auth; logging; misc tweaks (#41)
    * Integrate aws-c-auth; logging; misc tweaks

--- cmake/aws-crt-cpp-config.cmake
+++ cmake/aws-crt-cpp-config.cmake
@@ -3,5 +3,6 @@ include(CMakeFindDependencyMacro)
 find_dependency(aws-c-http)
 find_dependency(aws-c-mqtt)
 find_dependency(aws-c-cal)
+find_dependency(aws-c-auth)
 
 include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
--- source/Api.cpp
+++ source/Api.cpp
@@ -17,6 +17,7 @@
 #include <aws/crt/external/cJSON.h>
 #include <aws/crt/io/TlsOptions.h>
 
+#include <aws/auth/auth.h>
 #include <aws/http/http.h>
 
 namespace Aws
@@ -36,6 +37,13 @@ namespace Aws
             Io::InitTlsStaticState(allocator);
             aws_http_library_init(allocator);
             aws_mqtt_library_init(allocator);
+            aws_auth_library_init(allocator);
+
+            aws_load_error_strings();
+            aws_io_load_error_strings();
+
+            aws_common_load_log_subject_strings();
+            aws_io_load_log_subject_strings();
 
             cJSON_Hooks hooks;
             hooks.malloc_fn = s_cJSONAlloc;
@@ -46,21 +54,68 @@ namespace Aws
         static void s_cleanUpApi()
         {
             g_allocator = nullptr;
+            aws_auth_library_clean_up();
             aws_mqtt_library_clean_up();
             aws_http_library_clean_up();
             Io::CleanUpTlsStaticState();
         }
 
-        ApiHandle::ApiHandle(Allocator *allocator) noexcept { s_initApi(allocator); }
+        ApiHandle::ApiHandle(Allocator *allocator) noexcept : logger() { s_initApi(allocator); }
 
-        ApiHandle::ApiHandle() noexcept { s_initApi(DefaultAllocator()); }
+        ApiHandle::ApiHandle() noexcept : logger() { s_initApi(DefaultAllocator()); }
 
-        ApiHandle::~ApiHandle() { s_cleanUpApi(); }
-
-        void LoadErrorStrings() noexcept
+        ApiHandle::~ApiHandle()
         {
-            aws_load_error_strings();
-            aws_io_load_error_strings();
+            s_cleanUpApi();
+
+            if (aws_logger_get() == &logger)
+            {
+                aws_logger_set(NULL);
+                aws_logger_clean_up(&logger);
+            }
+        }
+
+        void ApiHandle::InitializeLogging(Aws::Crt::LogLevel level, const char *filename)
+        {
+            struct aws_logger_standard_options options;
+            AWS_ZERO_STRUCT(options);
+
+            options.level = (enum aws_log_level)level;
+            options.filename = filename;
+
+            InitializeLoggingCommon(options);
+        }
+
+        void ApiHandle::InitializeLogging(Aws::Crt::LogLevel level, FILE *fp)
+        {
+            struct aws_logger_standard_options options;
+            AWS_ZERO_STRUCT(options);
+
+            options.level = (enum aws_log_level)level;
+            options.file = fp;
+
+            InitializeLoggingCommon(options);
+        }
+
+        void ApiHandle::InitializeLoggingCommon(struct aws_logger_standard_options &options)
+        {
+            if (aws_logger_get() == &logger)
+            {
+                aws_logger_set(NULL);
+                aws_logger_clean_up(&logger);
+                if (options.level == AWS_LL_NONE)
+                {
+                    AWS_ZERO_STRUCT(logger);
+                    return;
+                }
+            }
+
+            if (aws_logger_init_standard(&logger, g_allocator, &options))
+            {
+                return;
+            }
+
+            aws_logger_set(&logger);
         }
 
         const char *ErrorDebugString(int error) noexcept { return aws_error_debug_str(error); }
