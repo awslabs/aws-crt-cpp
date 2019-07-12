6e3a30cc115a924511a7fd96fbe401f8cb625498
Bret Ambrose <bretambrose@gmail.com>
2019-07-12 11:01:29 -0700

    Wrap aws_http_request (#44)
    * Wrap aws_http_request

---
+++ include/aws/crt/http/HttpRequest.h
@@ -0,0 +1,70 @@
+#pragma once
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/crt/Exports.h>
+#include <aws/crt/Types.h>
+
+struct aws_http_header;
+struct aws_http_request;
+
+namespace Aws
+{
+    namespace Crt
+    {
+        namespace Http
+        {
+            using HttpHeader = aws_http_header;
+
+            /*
+             * Class representing a mutable http request.
+             */
+            class AWS_CRT_CPP_API HttpRequest
+            {
+              public:
+                HttpRequest(Allocator *allocator = DefaultAllocator()) noexcept;
+                ~HttpRequest();
+
+                HttpRequest(const HttpRequest &) = delete;
+                HttpRequest(HttpRequest &&) = delete;
+                HttpRequest &operator=(const HttpRequest &) = delete;
+                HttpRequest &operator=(HttpRequest &&) = delete;
+
+                Optional<ByteCursor> GetMethod() const noexcept;
+                bool SetMethod(ByteCursor method) noexcept;
+
+                Optional<ByteCursor> GetPath() const noexcept;
+                bool SetPath(ByteCursor path) noexcept;
+
+                std::shared_ptr<Aws::Crt::Io::IStream> GetBody() const noexcept;
+                bool SetBody(const std::shared_ptr<Aws::Crt::Io::IStream> &body) noexcept;
+
+                size_t GetHeaderCount() const noexcept;
+                Optional<HttpHeader> GetHeader(size_t index) const noexcept;
+                bool SetHeader(size_t index, const HttpHeader &header) noexcept;
+                bool AddHeader(const HttpHeader &header) noexcept;
+                bool EraseHeader(size_t index) noexcept;
+
+                operator bool() const noexcept { return m_request != nullptr; }
+
+              private:
+                Allocator *m_allocator;
+
+                struct aws_http_request *m_request;
+                std::shared_ptr<Aws::Crt::Io::IStream> m_bodyStream;
+            };
+        } // namespace Http
+    }     // namespace Crt
+} // namespace Aws
---
+++ tests/HttpRequestTest.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+#include <aws/crt/Api.h>
+
+#include <aws/crt/http/HttpRequest.h>
+
+#include <aws/http/request_response.h>
+
+#include <aws/testing/aws_test_harness.h>
+
+#include <sstream>
+
+using namespace Aws::Crt::Http;
+
+static int s_HttpRequestTestCreateDestroy(struct aws_allocator *allocator, void *ctx)
+{
+    (void)ctx;
+    Aws::Crt::ApiHandle apiHandle(allocator);
+
+    {
+        Aws::Crt::Http::HttpRequest request;
+        request.SetMethod(aws_byte_cursor_from_c_str("GET"));
+        request.SetPath(aws_byte_cursor_from_c_str("/index"));
+
+        std::shared_ptr<Aws::Crt::Io::IStream> stream = std::make_shared<std::stringstream>("TestContent");
+        request.SetBody(stream);
+
+        std::shared_ptr<Aws::Crt::Io::IStream> stream2 = std::make_shared<std::stringstream>("SomeOtherContent");
+        request.SetBody(stream2);
+
+        HttpHeader header1 = {aws_byte_cursor_from_c_str("Host"), aws_byte_cursor_from_c_str("www.test.com")};
+        request.AddHeader(header1);
+
+        HttpHeader header2 = {aws_byte_cursor_from_c_str("Authorization"), aws_byte_cursor_from_c_str("sadf")};
+        request.AddHeader(header2);
+
+        HttpHeader header3 = {aws_byte_cursor_from_c_str("UserAgent"), aws_byte_cursor_from_c_str("unit-tests-1.0")};
+        request.AddHeader(header3);
+
+        request.EraseHeader(2);
+    }
+
+    return AWS_OP_SUCCESS;
+}
+
+AWS_TEST_CASE(HttpRequestTestCreateDestroy, s_HttpRequestTestCreateDestroy)
