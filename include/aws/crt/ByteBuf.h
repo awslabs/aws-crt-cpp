7d286e616e3370c8fc06187c81f892be0019ecaa
Bret Ambrose <bambrose@amazon.com>
2019-08-08 15:09:26 -0700

    Initial API proposal

---
+++ include/aws/crt/ByteBuf.h
@@ -0,0 +1,119 @@
+#pragma once
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/crt/Outcome.h>
+#include <aws/crt/Types.h>
+
+#include <aws/common/byte_buf.h>
+
+namespace Aws
+{
+    namespace Crt
+    {
+
+        class ByteCursor
+        {
+          public:
+            ByteCursor() noexcept;
+            ByteCursor(const ByteCursor &cursor) noexcept;
+            explicit ByteCursor(const char *str) noexcept;
+            explicit ByteCursor(const String &str) noexcept;
+            explicit ByteCursor(aws_byte_cursor cursor) noexcept;
+            ByteCursor(const uint8_t *array, size_t len) noexcept;
+
+            ByteCursor &operator=(const ByteCursor &cursor) noexcept;
+
+            aws_byte_cursor Get() const noexcept { return m_cursor; }
+            aws_byte_cursor *GetPtr() noexcept { return &m_cursor; }
+
+          private:
+            aws_byte_cursor m_cursor;
+        };
+
+        class ByteBuf
+        {
+          public:
+            virtual ~ByteBuf() {}
+
+            // All of non-init API
+            AwsCrtResultVoid Append(ByteCursor cursor) noexcept;
+            AwsCrtResultVoid AppendDynamic(ByteCursor cursor) noexcept;
+            // etc...
+
+            virtual aws_byte_buf *Get() noexcept = 0;
+            virtual const aws_byte_buf *Get() const noexcept = 0;
+
+          protected:
+            ByteBuf() noexcept {}
+
+            ByteBuf(const ByteBuf &rhs) noexcept = delete;
+            ByteBuf(ByteBuf &rhs) noexcept = delete;
+
+            ByteBuf &operator=(const ByteBuf &buffer) noexcept = delete;
+            ByteBuf &operator=(ByteBuf &&buffer) noexcept = delete;
+        };
+
+        class ByteBufRef : public ByteBuf
+        {
+          public:
+            ByteBufRef() : m_buffer(nullptr) {}
+            ByteBufRef(const ByteBufRef &ref) : m_buffer(ref.m_buffer) {}
+            ByteBufRef(ByteBufRef &&ref) : m_buffer(ref.m_buffer) {}
+            explicit ByteBufRef(aws_byte_buf *buffer) : m_buffer(buffer) {}
+
+            virtual ~ByteBufRef() {}
+
+            ByteBufRef &operator=(const ByteBufRef &buffer) noexcept;
+            ByteBufRef &operator=(ByteBufRef &&buffer) noexcept;
+
+            virtual aws_byte_buf *Get() noexcept override { return m_buffer; }
+            virtual const aws_byte_buf *Get() const noexcept override { return m_buffer; }
+
+          private:
+            aws_byte_buf *m_buffer;
+        };
+
+        class ByteBufValue : public ByteBuf
+        {
+          public:
+            ByteBufValue() noexcept;
+            ByteBufValue(const ByteBufValue &buffer) noexcept = delete;
+            ByteBufValue(ByteBufValue &&buffer) noexcept;
+            explicit ByteBufValue(const char *str) noexcept;
+            ByteBufValue(const uint8_t *array, size_t capacity, size_t len);
+
+            virtual ~ByteBufValue();
+
+            ByteBufValue &operator=(ByteBufValue &&buffer) noexcept;
+            ByteBufValue &operator=(const ByteBufValue &buffer) noexcept = delete;
+
+            static AwsCrtResult<ByteBufValue> Init(const ByteBufValue &buffer) noexcept;
+            static AwsCrtResult<ByteBufValue> Init(Allocator *alloc, size_t capacity) noexcept;
+            static AwsCrtResult<ByteBufValue> InitFromArray(
+                Allocator *alloc,
+                const uint8_t *array,
+                size_t len) noexcept;
+
+            virtual aws_byte_buf *Get() noexcept override { return &m_buffer; }
+            virtual const aws_byte_buf *Get() const noexcept override { return &m_buffer; }
+
+          private:
+            void Cleanup() noexcept;
+
+            struct aws_byte_buf m_buffer;
+        };
+    } // namespace Crt
+} // namespace Aws
---
+++ source/ByteBuf.cpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <aws/crt/ByteBuf.h>
+
+namespace Aws
+{
+    namespace Crt
+    {
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        ByteCursor::ByteCursor() noexcept { AWS_ZERO_STRUCT(m_cursor); }
+
+        ByteCursor::ByteCursor(const char *str) noexcept { m_cursor = aws_byte_cursor_from_c_str(str); }
+
+        ByteCursor::ByteCursor(const String &str) noexcept
+        {
+            m_cursor = aws_byte_cursor_from_array(str.c_str(), str.size());
+        }
+
+        ByteCursor::ByteCursor(aws_byte_cursor cursor) noexcept : m_cursor(cursor) {}
+
+        ByteCursor::ByteCursor(const uint8_t *array, size_t len) noexcept
+        {
+            m_cursor = aws_byte_cursor_from_array(array, len);
+        }
+
+        ByteCursor::ByteCursor(const ByteCursor &cursor) noexcept { m_cursor = cursor.m_cursor; }
+
+        ByteCursor &ByteCursor::operator=(const ByteCursor &cursor) noexcept
+        {
+            m_cursor = cursor.m_cursor;
+
+            return *this;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        AwsCrtResultVoid ByteBuf::Append(ByteCursor cursor) noexcept
+        {
+            struct aws_byte_buf *buffer = Get();
+            if (aws_byte_buf_append(buffer, cursor.GetPtr()))
+            {
+                return MakeLastErrorResult<void>();
+            }
+
+            return AwsCrtResultVoid();
+        }
+
+        AwsCrtResultVoid ByteBuf::AppendDynamic(ByteCursor cursor) noexcept
+        {
+            struct aws_byte_buf *buffer = Get();
+            if (aws_byte_buf_append_dynamic(buffer, cursor.GetPtr()))
+            {
+                return MakeLastErrorResult<void>();
+            }
+
+            return AwsCrtResultVoid();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        ByteBufRef &ByteBufRef::operator=(const ByteBufRef &buffer) noexcept
+        {
+            m_buffer = buffer.m_buffer;
+            return *this;
+        }
+
+        ByteBufRef &ByteBufRef::operator=(ByteBufRef &&buffer) noexcept
+        {
+            m_buffer = buffer.m_buffer;
+            return *this;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        ByteBufValue::ByteBufValue() noexcept { AWS_ZERO_STRUCT(m_buffer); }
+
+        ByteBufValue::ByteBufValue(ByteBufValue &&buffer) noexcept : m_buffer(buffer.m_buffer)
+        {
+            AWS_ZERO_STRUCT(buffer.m_buffer);
+        }
+
+        ByteBufValue::ByteBufValue(const char *str) noexcept : m_buffer(aws_byte_buf_from_c_str(str)) {}
+
+        ByteBufValue::ByteBufValue(const uint8_t *array, size_t capacity, size_t len)
+            : m_buffer(aws_byte_buf_from_array(array, capacity))
+        {
+            AWS_FATAL_ASSERT(len <= capacity);
+            m_buffer.len = len;
+        }
+
+        ByteBufValue::~ByteBufValue() { Cleanup(); }
+
+        ByteBufValue &ByteBufValue::operator=(ByteBufValue &&buffer) noexcept
+        {
+            if (&buffer != this)
+            {
+                Cleanup();
+                m_buffer = buffer.m_buffer;
+                AWS_ZERO_STRUCT(buffer.m_buffer);
+            }
+
+            return *this;
+        }
+
+        AwsCrtResult<ByteBufValue> ByteBufValue::Init(const ByteBufValue &buffer) noexcept
+        {
+            ByteBufValue temp;
+
+            if (buffer.m_buffer.allocator != nullptr)
+            {
+                if (aws_byte_buf_init_copy(&temp.m_buffer, buffer.m_buffer.allocator, &buffer.m_buffer))
+                {
+                    return MakeLastErrorResult<ByteBufValue>();
+                }
+            }
+            else
+            {
+                temp.m_buffer = buffer.m_buffer;
+            }
+
+            return AwsCrtResult<ByteBufValue>(std::move(temp));
+        }
+
+        AwsCrtResult<ByteBufValue> ByteBufValue::Init(Allocator *alloc, size_t capacity) noexcept
+        {
+            ByteBufValue temp;
+
+            if (aws_byte_buf_init(&temp.m_buffer, alloc, capacity))
+            {
+                return MakeLastErrorResult<ByteBufValue>();
+            }
+
+            return AwsCrtResult<ByteBufValue>(std::move(temp));
+        }
+
+        AwsCrtResult<ByteBufValue> ByteBufValue::InitFromArray(
+            Allocator *alloc,
+            const uint8_t *array,
+            size_t len) noexcept
+        {
+            ByteBufValue temp;
+
+            struct aws_byte_cursor cursor = {len, const_cast<uint8_t *>(array)};
+            if (aws_byte_buf_init_copy_from_cursor(&temp.m_buffer, alloc, cursor))
+            {
+                return MakeLastErrorResult<ByteBufValue>();
+            }
+
+            return AwsCrtResult<ByteBufValue>(std::move(temp));
+        }
+
+        void ByteBufValue::Cleanup() noexcept { aws_byte_buf_clean_up(&m_buffer); }
+    } // namespace Crt
+} // namespace Aws
