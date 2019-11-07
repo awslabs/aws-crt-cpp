6a9cd368a210dbf858f09a149dbed99e73922519
Jonathan M. Henson <henso@amazon.com>
2019-11-06 21:55:55 -0800

    Supress unnecessary output.

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -6,7 +6,7 @@ REM Building deps to %TEMP% to avoid max path length errors during build
 cmake %* -DBUILD_DEPS=ON  -DAWS_DEPS_BUILD_DIR="%TEMP%" -DCMAKE_BUILD_TYPE="Release" ../ || goto error
 msbuild.exe aws-crt-cpp.vcxproj /p:Configuration=Release || goto error
 msbuild.exe tests/aws-crt-cpp-tests.vcxproj /p:Configuration=Release
-ctest -V || goto error
+ctest --output-on-failure || goto error
 
 goto :EOF
 
