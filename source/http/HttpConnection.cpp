ec363925fdf2c33d1c85bfb65ff0a6e67d01acf7
Jonathan M. Henson <henso@amazon.com>
2019-11-26 15:06:35 -0800

    Working cloud watch publishing.

--- source/http/HttpConnection.cpp
+++ source/http/HttpConnection.cpp
@@ -139,9 +139,6 @@ namespace Aws
             std::shared_ptr<HttpClientStream> HttpClientConnection::NewClientStream(
                 const HttpRequestOptions &requestOptions) noexcept
             {
-                AWS_ASSERT(requestOptions.onIncomingHeaders);
-                AWS_ASSERT(requestOptions.onStreamComplete);
-
                 aws_http_make_request_options options;
                 AWS_ZERO_STRUCT(options);
                 options.self_size = sizeof(aws_http_make_request_options);
@@ -208,7 +205,11 @@ namespace Aws
                 void *userData) noexcept
             {
                 auto callbackData = static_cast<ClientStreamCallbackData *>(userData);
-                callbackData->stream->m_onIncomingHeaders(*callbackData->stream, headerBlock, headerArray, numHeaders);
+                if (callbackData->stream->m_onIncomingHeaders)
+                {
+                    callbackData->stream->m_onIncomingHeaders(
+                        *callbackData->stream, headerBlock, headerArray, numHeaders);
+                }
 
                 return AWS_OP_SUCCESS;
             }
@@ -246,7 +247,11 @@ namespace Aws
             void HttpStream::s_onStreamComplete(struct aws_http_stream *, int errorCode, void *userData) noexcept
             {
                 auto callbackData = static_cast<ClientStreamCallbackData *>(userData);
-                callbackData->stream->m_onStreamComplete(*callbackData->stream, errorCode);
+                if (callbackData->stream->m_onStreamComplete)
+                {
+                    callbackData->stream->m_onStreamComplete(*callbackData->stream, errorCode);
+                }
+
                 callbackData->stream = nullptr;
             }
 
