6b07aa56a6e52de43ad2961a1157982f2a568c10
Ryan Carper <rccarper@amazon.com>
2020-03-26 14:30:52 -0400

    Moving MeasureTransferRateStream to its own file

--- canary/MeasureTransferRate.cpp
+++ canary/MeasureTransferRate.cpp
@@ -13,10 +13,6 @@
 
 using namespace Aws::Crt;
 
-// TODO make handling of BodyTemplate less awkward.
-size_t MeasureTransferRate::BodyTemplateSize = 4ULL * 1024ULL;
-thread_local char *BodyTemplate = nullptr;
-
 const uint64_t MeasureTransferRate::SmallObjectSize = 5ULL * 1024ULL * 1024ULL * 1024ULL;
 const uint32_t MeasureTransferRate::LargeObjectNumParts = 8192;
 const uint64_t MeasureTransferRate::LargeObjectSize =
@@ -27,98 +23,6 @@ const uint64_t MeasureTransferRate::AllocationMetricFrequencyNS = aws_timestamp_
     AWS_TIMESTAMP_MILLIS,
     AWS_TIMESTAMP_NANOS,
     NULL);
-
-bool MeasureTransferRateStream::IsValid() const noexcept
-{
-    return true; // TODO return !is_end_of_stream?
-}
-
-bool MeasureTransferRateStream::ReadImpl(ByteBuf &dest) noexcept
-{
-    if (BodyTemplate == nullptr)
-    {
-        char BodyTemplateData[] =
-            "This is a test string for use with canary testing against Amazon Simple Storage Service";
-
-        BodyTemplate = new char[MeasureTransferRate::BodyTemplateSize];
-        BodyTemplate[MeasureTransferRate::BodyTemplateSize - 1] = '\0';
-
-        size_t totalToWrite = MeasureTransferRate::BodyTemplateSize;
-        char *BodyTemplatePos = BodyTemplate;
-
-        while (totalToWrite)
-        {
-            size_t toWrite = AWS_ARRAY_SIZE(BodyTemplateData) - 1 > totalToWrite ? totalToWrite
-                                                                                 : AWS_ARRAY_SIZE(BodyTemplateData) - 1;
-
-            memcpy(BodyTemplatePos, BodyTemplateData, toWrite);
-
-            BodyTemplatePos += toWrite;
-            totalToWrite -= toWrite;
-        }
-    }
-
-    size_t totalBufferSpace = dest.capacity - dest.len;
-    size_t unwritten = m_transferState->GetSizeInBytes() - m_written;
-    size_t totalToWrite = totalBufferSpace > unwritten ? unwritten : totalBufferSpace;
-    size_t writtenOut = 0;
-
-    if (m_written == 0)
-    {
-        m_timestamp = DateTime::Now();
-    }
-
-    while (totalToWrite)
-    {
-        size_t toWrite = MeasureTransferRate::BodyTemplateSize - 1 > totalToWrite
-                             ? totalToWrite
-                             : MeasureTransferRate::BodyTemplateSize - 1;
-        ByteCursor outCur = ByteCursorFromArray((const uint8_t *)BodyTemplate, toWrite);
-
-        aws_byte_buf_append(&dest, &outCur);
-
-        writtenOut += toWrite;
-        totalToWrite = totalToWrite - toWrite;
-    }
-
-    m_written += writtenOut;
-
-    // A quick way for us to measure how much data we've actually written to S3 storage.  (This working is reliant
-    // on this function only being used when we are reading data from the stream while sending that data to S3.)
-    m_transferState->AddDataUpMetric(writtenOut);
-
-    return true;
-}
-
-Io::StreamStatus MeasureTransferRateStream::GetStatusImpl() const noexcept
-{
-    Io::StreamStatus status;
-    status.is_end_of_stream = m_written == m_transferState->GetSizeInBytes();
-    status.is_valid = !status.is_end_of_stream;
-
-    return status;
-}
-
-bool MeasureTransferRateStream::SeekImpl(Io::OffsetType, Io::StreamSeekBasis) noexcept
-{
-    m_written = 0;
-    return true;
-}
-
-int64_t MeasureTransferRateStream::GetLengthImpl() const noexcept
-{
-    return m_transferState->GetSizeInBytes();
-}
-
-MeasureTransferRateStream::MeasureTransferRateStream(
-    CanaryApp &canaryApp,
-    const std::shared_ptr<TransferState> &transferState,
-    Allocator *allocator)
-    : m_canaryApp(canaryApp), m_transferState(transferState), m_allocator(allocator), m_written(0)
-{
-    (void)m_canaryApp;
-    (void)m_allocator;
-}
 
 MeasureTransferRate::MeasureTransferRate(CanaryApp &canaryApp) : m_canaryApp(canaryApp)
 {
