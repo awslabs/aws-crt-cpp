1e5cc45c0b3032d135a84cb3e9b7fa4eb761c82c
Justin Boswell <boswej@amazon.com>
2020-06-17 23:16:22 +0000

    Fixed sed regex

--- update-version.sh
+++ update-version.sh
@@ -10,7 +10,7 @@ GITHUB_TOKEN=$1
 pushd $(dirname $0) > /dev/null
 
 version=$(git describe --tags --abbrev=0)
-sed --in-place -e "s/set(AWS_CRT_CPP_VERSION \".+\")/set(AWS_CRT_CPP_VERSION \"${version}\")/" CMakeLists.txt
+sed --in-place -r -e "s/set\\(AWS_CRT_CPP_VERSION \".+\"\\)/set(AWS_CRT_CPP_VERSION \"${version}\")/" CMakeLists.txt
 echo "Updating AWS_CRT_CPP_VERSION default to ${version}"
 
 if git diff --exit-code CMakeLists.txt > /dev/null; then
