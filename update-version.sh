d1b2f0ffbaeb92ca54f3bfe024a7aa4b1e48a927
Justin Boswell <boswej@amazon.com>
2020-06-17 23:12:07 +0000

    Fixing automated push

--- update-version.sh
+++ update-version.sh
@@ -10,7 +10,7 @@ GITHUB_TOKEN=$1
 pushd $(dirname $0) > /dev/null
 
 version=$(git describe --tags --abbrev=0)
-sed --in-place -e "s/set(AWS_CRT_CPP_VERSION \"v1.0.0-dev\")/set(AWS_CRT_CPP_VERSION \"${version}\")/" CMakeLists.txt
+sed --in-place -e "s/set(AWS_CRT_CPP_VERSION \".+\")/set(AWS_CRT_CPP_VERSION \"${version}\")/" CMakeLists.txt
 echo "Updating AWS_CRT_CPP_VERSION default to ${version}"
 
 if git diff --exit-code CMakeLists.txt > /dev/null; then
@@ -21,7 +21,7 @@ else
     git add CMakeLists.txt
     git commit -m "Updated version to ${version}"
     git tag -f ${version}
-    git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/awslabs/aws-crt-cpp.git" --force-with-lease --tags --follow-tags
+    git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/awslabs/aws-crt-cpp.git" HEAD:master --force-with-lease --tags --follow-tags
 fi
 
 popd > /dev/null
