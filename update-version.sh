a920d40fb10a76f3d12163142265b6e8f726f9da
Justin Boswell <boswej@amazon.com>
2020-06-17 15:11:48 -0700

    Added a job that updates version in CMakeLists.txt when a new tag is created (#128)

---
+++ update-version.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Redirect output to stderr.
+exec 1>&2
+
+pushd $(dirname $0) > /dev/null
+
+version=$(git describe --tags --abbrev=0)
+sed --in-place -e "s/set(AWS_CRT_CPP_VERSION \"v1.0.0-dev\")/set(AWS_CRT_CPP_VERSION \"${version}\")" CMakeLists.txt
+echo "Updating AWS_CRT_CPP_VERSION default to ${version}"
+
+git diff --exit-code CMakeLists.txt > /dev/null
+if [ $? -eq 1 ]; then
+    git add CMakeLists.txt
+    git commit -m "Updated version to ${version}"
+    git tag -f ${version}
+    git push --follow-tags
+fi
+
+popd > /dev/null
