bf08a3a895b47a562a1c120e599a4789ed7e0933
Andrew Tang <tangandr@amazon.com>
2020-09-09 16:06:47 -0700

    Address reviews

--- include/aws/crt/ImdsClient.h
+++ include/aws/crt/ImdsClient.h
@@ -9,12 +9,6 @@
 #include <aws/crt/Types.h>
 #include <functional>
 
-struct aws_array_list;
-struct aws_credentials;
-struct aws_imds_client;
-struct aws_imds_iam_profile;
-struct aws_imds_instance_info;
-
 namespace Aws
 {
 
