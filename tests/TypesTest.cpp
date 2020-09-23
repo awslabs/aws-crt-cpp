291905c2ffe3704e446798daef471b5e51974540
Andrew Tang <tangandr@amazon.com>
2020-09-23 13:09:15 -0700

    test

--- tests/TypesTest.cpp
+++ tests/TypesTest.cpp
@@ -1,6 +1,6 @@
 /**
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
+ * SPDX-License-Identifier: Apache-2.0..
  */
 #include <aws/crt/Api.h>
 #include <aws/crt/Types.h>
