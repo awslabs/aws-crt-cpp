fc4f16ca78b2f6734e3d9e79b071c7a00867d9d6
Michael Graeb <graebm@amazon.com>
2020-12-08 14:11:33 -0800

    attempt to fix c++20 msvc errors

--- include/aws/crt/StringView.h
+++ include/aws/crt/StringView.h
@@ -843,16 +843,12 @@ namespace Aws
 // hash
 namespace std
 {
-    template <class CharT, class StringViewTraits> struct hash<Aws::Crt::basic_string_view<CharT, StringViewTraits>>
+    template <class CharT, class Traits> struct hash<Aws::Crt::basic_string_view<CharT, Traits>>
     {
-        size_t operator()(const Aws::Crt::basic_string_view<CharT, StringViewTraits> &val) const noexcept;
+        size_t operator()(const Aws::Crt::basic_string_view<CharT, Traits> &val) const noexcept {
+            auto str = std::basic_string<CharT, Traits>(val.data(), val.size());
+            return std::hash<std::basic_string<CharT, Traits>>()(str);
+        }
     };
 
-    template <class CharT, class StringViewTraits>
-    size_t hash<Aws::Crt::basic_string_view<CharT, StringViewTraits>>::operator()(
-        const Aws::Crt::basic_string_view<CharT, StringViewTraits> &val) const noexcept
-    {
-        auto str = std::basic_string<CharT, StringViewTraits>(val.data(), val.size());
-        return std::hash<std::basic_string<CharT, StringViewTraits>>()(str);
-    }
 } // namespace std
