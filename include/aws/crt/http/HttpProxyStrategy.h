f9b6bced6e702c4bf05e60ad971b61127e8311de
Abhishek Jain <ajainaus@amazon.com>
2021-01-10 22:57:44 -0600

    add def for functions in user area

--- include/aws/crt/http/HttpProxyStrategy.h
+++ include/aws/crt/http/HttpProxyStrategy.h
@@ -10,6 +10,12 @@
 
 #include <aws/http/proxy_strategy.h>
 
+void send_kerberos_status(int httpStatusCode);
+char *get_kerb_usertoken();
+char *get_ntlm_resp();
+char *get_ntlm_cred();
+void send_ntlm_chall_header(size_t length, uint8_t *httpHeader, size_t length1, uint8_t *httpHeader1, size_t num_headers);
+
 namespace Aws
 {
     namespace Crt
@@ -41,6 +47,9 @@ namespace Aws
                 static std::shared_ptr<HttpProxyStrategyFactory> CreateKerberosHttpProxyStrategyFactory(
                     enum aws_http_proxy_connection_type connectionType,
                     const String &usertoken,
+                    Allocator *allocator = g_allocator);
+
+                static std::shared_ptr<HttpProxyStrategyFactory> CreateAdaptiveNtlmHttpProxyStrategyFactory(
                     Allocator *allocator = g_allocator);
 
               private:
