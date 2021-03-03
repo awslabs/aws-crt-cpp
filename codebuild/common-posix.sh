ac08d944910f9ba13bf2d84c89da4c556b331bcf
Bret Ambrose <bretambrose@gmail.com>
2021-03-02 17:27:59 -0800

    Test proxy reachability from codebuild

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -6,6 +6,7 @@ if test -f "/tmp/setup_proxy_test_env.sh"; then
     source /tmp/setup_proxy_test_env.sh
     env
     ls /tmp
+    curl --verbose --proxy http://ec2-100-25-139-228.compute-1.amazonaws.com:3128 www.example.com
 fi
 
 git submodule update --init
