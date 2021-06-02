8a4dd3a24b024010a328a31f7dbbb0968477aaa8
Ryan Carper <rccarper@amazon.com>
2021-06-02 16:02:28 -0400

    Fix for connection manager proxy test

--- tests/ProxyTest.cpp
+++ tests/ProxyTest.cpp
@@ -353,7 +353,7 @@ static int s_TestConnectionManagerTunnelingProxyHttpsInvalidTlsOptions(struct aw
 
         /* Reset TLS Options, making them invalid. */
         testState.m_connectionOptions.ProxyOptions->TlsOptions = TlsConnectionOptions();
-        ASSERT_FALSE(*testState.m_proxyOptions.TlsOptions);
+        ASSERT_FALSE(*testState.m_connectionOptions.ProxyOptions->TlsOptions);
 
         s_InitializeProxiedConnectionManager(testState, aws_byte_cursor_from_string(s_https_endpoint));
 
