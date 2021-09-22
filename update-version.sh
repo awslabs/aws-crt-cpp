5a4d5ac863b0c148b89488aab04c6725cb55f43f
Bret Ambrose <bretambrose@gmail.com>
2021-09-22 15:11:14 -0700

    Try new way of tagging releases; currently stuck on a version 1.5 years old (#278)
    * Try new way of tagging releases; currently stuck on a version 1.5 years old
    * Forward tag message properly

--- update-version.sh
+++ update-version.sh
@@ -10,7 +10,7 @@ GITHUB_TOKEN=$1
 
 pushd $(dirname $0) > /dev/null
 
-version=$(git describe --tags --abbrev=0)
+version=$(git tag --sort=-creatordate | head -n1)
 sed --in-place -r -e "s/set\\(AWS_CRT_CPP_VERSION \".+\"\\)/set(AWS_CRT_CPP_VERSION \"${version}\")/" CMakeLists.txt
 echo "Updating AWS_CRT_CPP_VERSION default to ${version}"
 
@@ -21,8 +21,20 @@ else
     git config --local user.name "GitHub Actions"
     git add CMakeLists.txt
     git commit -m "Updated version to ${version}"
-    git tag -f ${version}
-    git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/awslabs/aws-crt-cpp.git" --force --tags
+
+    # awkward - we need to snip the old tag message and then force overwrite the tag with the new commit but
+    # preserving the old message
+    tag_message=$(git tag -l -n20 ${version} | sed -n "s/${version} \(.*\)/\1/p")
+    echo "Old tag message is: ${tag_message}"
+
+    # delete the old tag on github
+    git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/awslabs/aws-crt-cpp.git" :refs/tags/${version}
+
+    # create new tag on latest commit with old message
+    git tag -f ${version} -m "${tag_message}"
+
+    # push new tag and commit to github
+    git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/awslabs/aws-crt-cpp.git" main --force --tags
 fi
 
 popd > /dev/null
