9640b5e628b06f5282e8cf4005fb25caeffe7793
Zhihui Xia <zhvxia@amazon.com>
2022-11-28 18:22:45 +0000

    quick test with proxy test setup

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -6,6 +6,10 @@ if test -f "/tmp/setup_proxy_test_env.sh"; then
     source /tmp/setup_proxy_test_env.sh
 fi
 
+
+# Check if environment variable get set
+echo $AWS_TEST_MQTT5_DIRECT_MQTT_PORT
+
 git submodule update --init
 mkdir build
 cd build
