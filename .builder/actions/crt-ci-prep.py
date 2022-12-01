d3211f44ef3cd34af636f69e88baa638c3fc22bf
Zhihui Xia <zhvxia@amazon.com>
2022-12-01 09:12:24 -0800

    revert test changes

--- .builder/actions/crt-ci-prep.py
+++ .builder/actions/crt-ci-prep.py
@@ -12,11 +12,6 @@ class CrtCiPrep(Builder.Action):
         env.shell.setenv("AWS_TESTING_COGNITO_IDENTITY", env.shell.get_secret("aws-c-auth-testing/cognito-identity"))
         env.shell.setenv("AWS_TESTING_STS_ROLE_ARN", env.shell.get_secret("aws-c-auth-testing/sts-role-arn"))
 
-        env.shell.setenv("AWS_TEST_MQTT5_DIRECT_MQTT_HOST", "localhost")
-        env.shell.setenv("AWS_TEST_MQTT5_DIRECT_MQTT_PORT", "1883")
-        env.shell.setenv("AWS_TEST_MQTT5_PROXY_HOST", "localhost")
-        env.shell.setenv("AWS_TEST_MQTT5_PROXY_PORT", "1883")
-
         actions = []
 
         return Builder.Script(actions, name='crt-ci-prep')
