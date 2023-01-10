173e706be229c4c564632f5c89e14a4f80a0d512
Michael Graeb <graebm@amazon.com>
2023-01-10 00:14:04 +0000

    default elasticurl timeout 3s -> 5s

--- bin/elasticurl_cpp/main.cpp
+++ bin/elasticurl_cpp/main.cpp
@@ -28,7 +28,7 @@ struct ElasticurlCtx
     const char *CaPath = nullptr;
     const char *Cert = nullptr;
     const char *Key = nullptr;
-    int ConnectTimeout = 3000;
+    int ConnectTimeout = 5000;
     Vector<const char *> HeaderLines;
     const char *Alpn = "h2;http/1.1";
     bool IncludeHeaders = false;
