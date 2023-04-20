b3fe838ea35e33430c443e944b347009b5de4fdd
Zhihui Xia <zhvxia@amazon.com>
2023-04-20 15:09:12 -0700

    code review changes

--- include/aws/crt/mqtt/Mqtt5Listener.h
+++ include/aws/crt/mqtt/Mqtt5Listener.h
@@ -14,11 +14,12 @@ namespace Aws
         namespace Mqtt5
         {
             class Mqtt5ListenerOptions;
+            class Mqtt5ListenerCore;
 
             /**
              * Type signature of the callback invoked when a PacketPublish message received for Mqtt5ListenerCore.
              *
-             * @return true if the message get handled otherwise return false
+             * @return true if the message is handled, otherwise return false
              */
             using OnListenerPublishReceivedHandler = std::function<bool(const PublishReceivedEventData &)>;
 
@@ -28,147 +29,12 @@ namespace Aws
             using OnListenerTerminationHandler = std::function<void(void *)>;
 
             /**
-             * An MQTT5 listener core. The core is only internally used by Mqtt5Listener to manage the callbacks and
-             * underlying c objects.
-             *
-             * This is a move-only type. Unless otherwise specified, all function arguments need only to live through
-             * the duration of the function call.
-             */
-            class Mqtt5ListenerCore final : public std::enable_shared_from_this<Mqtt5ListenerCore>
-            {
-              public:
-                /**
-                 * Factory function for mqtt5 listener
-                 *
-                 * @param options: Mqtt5ListenerOption
-                 * @param client: Mqtt5Client to listen to
-                 * @param allocator allocator to use
-                 * @return a new mqtt5 listener core
-                 */
-                static std::shared_ptr<Mqtt5ListenerCore> NewMqtt5ListenerCore(
-                    const Mqtt5ListenerOptions &options,
-                    const std::shared_ptr<Mqtt5Client> &client,
-                    Allocator *allocator = ApiAllocator()) noexcept;
-
-                /**
-                 * Get shared pointer of the Mqtt5ListenerCore. Mqtt5ListenerCore is inherited to
-                 * enable_shared_from_this to help with memory safety.
-                 *
-                 * @return shared_ptr for the Mqtt5ListenerCore
-                 */
-                std::shared_ptr<Mqtt5ListenerCore> getptr() { return shared_from_this(); }
-
-                /**
-                 * @return true if the instance is in a valid state, false otherwise.
-                 */
-                operator bool() const noexcept;
-
-                /**
-                 * @return the value of the last aws error encountered by operations on this instance.
-                 */
-                int LastError() const noexcept;
-
-                /**
-                 * Tells the listener to release the native listener and clean up unhandled the resources
-                 * and operations before destroying it. You MUST only call this function when you want to
-                 * release the listener.
-                 * This is "an ugly and unfortunate necessity" before releasing the Mqtt5ListenerCore. And You
-                 * MUST call this function to avoid any future memory leaks or dead lock.
-                 *
-                 */
-                void Close() noexcept;
-
-                virtual ~Mqtt5ListenerCore();
-
-              private:
-                Mqtt5ListenerCore(
-                    const Mqtt5ListenerOptions &options,
-                    const std::shared_ptr<Mqtt5Client> &client,
-                    Allocator *allocator = ApiAllocator()) noexcept;
-
-                static void s_lifeCycleEventCallback(const aws_mqtt5_client_lifecycle_event *event);
-
-                static bool s_publishReceivedCallback(const aws_mqtt5_packet_publish_view *publish, void *user_data);
-
-                static void s_listenerTerminationCompletion(void *complete_ctx);
-
-                /**
-                 * Callback handler triggered when client successfully establishes an MQTT connection
-                 */
-                OnConnectionSuccessHandler onConnectionSuccess;
-
-                /**
-                 * Callback handler triggered when client fails to establish an MQTT connection
-                 */
-                OnConnectionFailureHandler onConnectionFailure;
-
-                /**
-                 * Callback handler triggered when client's current MQTT connection is closed
-                 */
-                OnDisconnectionHandler onDisconnection;
-
-                /**
-                 * Callback handler triggered when client reaches the "Stopped" state
-                 */
-                OnStoppedHandler onStopped;
-
-                /**
-                 * Callback handler triggered when client begins an attempt to connect to the remote endpoint.
-                 */
-                OnAttemptingConnectHandler onAttemptingConnect;
-
-                /**
-                 * Callback handler triggered when an MQTT PUBLISH packet is received by the client
-                 */
-                OnListenerPublishReceivedHandler onListenerPublishReceived;
-
-                /**
-                 * Callback handler triggered when listener terminated
-                 */
-                OnListenerTerminationHandler onListenerTermination;
-
-                void *termination_userdata;
-
-                /*
-                 * Reference to Mqtt5Client. As the listener has a dependency on mqtt5_client, we would like to keep the
-                 * Mqtt5Client alive.
-                 */
-                std::shared_ptr<Mqtt5Client> m_mqtt5Client;
-
-                /*
-                 * Self reference to keep self alive until the termination callback is invoked.
-                 */
-                std::shared_ptr<Mqtt5ListenerCore> m_selfReference;
-
-                /*
-                 * The callback flag used to indicate if it is safe to invoke the callbacks
-                 */
-                enum CallbackFlag
-                {
-                    INVOKE,
-                    IGNORE
-                } m_callbackFlag;
-
-                /*
-                 * Lock for the callbacks. This is used to protect the callback flag and callbacks.
-                 */
-                std::recursive_mutex m_callback_lock;
-
-                /**
-                 * Underlying c classes for data storage and operational members
-                 */
-                aws_mqtt5_listener *m_listener;
-
-                Allocator *m_allocator;
-            };
-
-            /**
              * An MQTT5 listener.
              *
-             * This is a move-only type. Unless otherwise specified, all function arguments need only to live through
-             * the duration of the function call.
+             * This is a move-only type. Unless otherwise specified, all function arguments only need to live through
+             * the duration of the function call
              */
-            class AWS_CRT_CPP_API Mqtt5Listener : public std::enable_shared_from_this<Mqtt5Listener>
+            class AWS_CRT_CPP_API Mqtt5Listener
             {
               public:
                 /**
@@ -177,20 +43,12 @@ namespace Aws
                  * @param options: Mqtt5ListenerOption
                  * @param client: Mqtt5Client to listen to
                  * @param allocator allocator to use
-                 * @return a new mqtt5 listener
+                 * @return a new mqtt5 listener, ScopedResource = std::unique_ptr<T, std::function<void(T *)>>;
                  */
-                static std::shared_ptr<Mqtt5Listener> NewMqtt5Listener(
+                static ScopedResource<Mqtt5Listener> NewMqtt5Listener(
                     const Mqtt5ListenerOptions &options,
                     const std::shared_ptr<Mqtt5Client> &client,
                     Allocator *allocator = ApiAllocator()) noexcept;
-
-                /**
-                 * Get shared pointer of the Mqtt5ListenerCore. Mqtt5ListenerCore is inherited to
-                 * enable_shared_from_this to help with memory safety.
-                 *
-                 * @return shared_ptr for the Mqtt5ListenerCore
-                 */
-                std::shared_ptr<Mqtt5Listener> getptr() { return shared_from_this(); }
 
                 /**
                  * @return true if the instance is in a valid state, false otherwise.
--- source/mqtt/Mqtt5Listener.cpp
+++ source/mqtt/Mqtt5Listener.cpp
@@ -10,6 +10,131 @@ namespace Aws
         namespace Mqtt5
         {
 
+            /**
+             * An MQTT5 listener core. The core is only used internally by Mqtt5Listener to manage the callbacks and
+             * underlying c objects.
+             *
+             * This is a move-only type. Unless otherwise specified, all function arguments only need to live through
+             * the duration of the function call.
+             */
+            class Mqtt5ListenerCore
+            {
+              public:
+                /**
+                 * Factory function for mqtt5 listener
+                 *
+                 * @param options: Mqtt5ListenerOption
+                 * @param client: Mqtt5Client to listen to
+                 * @param allocator allocator to use
+                 * @return a new mqtt5 listener core
+                 */
+                static std::shared_ptr<Mqtt5ListenerCore> NewMqtt5ListenerCore(
+                    const Mqtt5ListenerOptions &options,
+                    const std::shared_ptr<Mqtt5Client> &client,
+                    Allocator *allocator = ApiAllocator()) noexcept;
+
+                /**
+                 * @return true if the instance is in a valid state, false otherwise.
+                 */
+                operator bool() const noexcept;
+
+                /**
+                 * @return the value of the last aws error encountered by operations on this instance.
+                 */
+                int LastError() const noexcept;
+
+                /**
+                 * Tells the listener to release the native listener and clean up unhandled resources and operations
+                 * before destroying it. You MUST only call this function when you want to release the listener. This is
+                 * "an ugly and unfortunate necessity" before releasing the Mqtt5ListenerCore. You MUST call this
+                 * function to avoid memory leaks and/or dead locks.
+                 */
+                void Close() noexcept;
+
+                virtual ~Mqtt5ListenerCore();
+
+              private:
+                Mqtt5ListenerCore(
+                    const Mqtt5ListenerOptions &options,
+                    const std::shared_ptr<Mqtt5Client> &client,
+                    Allocator *allocator = ApiAllocator()) noexcept;
+
+                static void s_lifeCycleEventCallback(const aws_mqtt5_client_lifecycle_event *event);
+
+                static bool s_publishReceivedCallback(const aws_mqtt5_packet_publish_view *publish, void *user_data);
+
+                static void s_listenerTerminationCompletion(void *complete_ctx);
+
+                /**
+                 * Callback handler triggered when client successfully establishes an MQTT connection
+                 */
+                OnConnectionSuccessHandler onConnectionSuccess;
+
+                /**
+                 * Callback handler triggered when client fails to establish an MQTT connection
+                 */
+                OnConnectionFailureHandler onConnectionFailure;
+
+                /**
+                 * Callback handler triggered when client's current MQTT connection is closed
+                 */
+                OnDisconnectionHandler onDisconnection;
+
+                /**
+                 * Callback handler triggered when client reaches the "Stopped" state
+                 */
+                OnStoppedHandler onStopped;
+
+                /**
+                 * Callback handler triggered when client begins an attempt to connect to the remote endpoint.
+                 */
+                OnAttemptingConnectHandler onAttemptingConnect;
+
+                /**
+                 * Callback handler triggered when an MQTT PUBLISH packet is received by the client
+                 */
+                OnListenerPublishReceivedHandler onListenerPublishReceived;
+
+                /**
+                 * Callback handler triggered when listener terminated
+                 */
+                OnListenerTerminationHandler onListenerTermination;
+
+                void *termination_userdata;
+
+                /*
+                 * Reference to Mqtt5Client. As the listener has a dependency on mqtt5_client, we would like to keep the
+                 * Mqtt5Client alive.
+                 */
+                std::shared_ptr<Mqtt5Client> m_mqtt5Client;
+
+                /*
+                 * Self reference to keep self alive until the termination callback is invoked.
+                 */
+                std::shared_ptr<Mqtt5ListenerCore> m_selfReference;
+
+                /*
+                 * The callback flag used to indicate if it is safe to invoke the callbacks
+                 */
+                enum CallbackFlag
+                {
+                    INVOKE,
+                    IGNORE
+                } m_callbackFlag;
+
+                /*
+                 * Lock for the callbacks. This is used to protect the callback flag and callbacks.
+                 */
+                std::recursive_mutex m_callback_lock;
+
+                /**
+                 * Underlying c classes for data storage and operational members
+                 */
+                aws_mqtt5_listener *m_listener;
+
+                Allocator *m_allocator;
+            };
+
             std::shared_ptr<Mqtt5ListenerCore> Aws::Crt::Mqtt5::Mqtt5ListenerCore::NewMqtt5ListenerCore(
                 const Mqtt5ListenerOptions &options,
                 const std::shared_ptr<Mqtt5Client> &client,
@@ -18,7 +143,7 @@ namespace Aws
 
                 if (client == nullptr)
                 {
-                    AWS_LOGF_DEBUG(AWS_LS_MQTT5_GENERAL, "Failed to create Mqtt5ListenerCore. Invalid Client.");
+                    AWS_LOGF_ERROR(AWS_LS_MQTT5_GENERAL, "Failed to create Mqtt5ListenerCore. Invalid Client.");
                     aws_raise_error(AWS_ERROR_INVALID_ARGUMENT);
                     return nullptr;
                 }
@@ -42,7 +167,7 @@ namespace Aws
 
                 std::shared_ptr<Mqtt5ListenerCore> shared_listener = std::shared_ptr<Mqtt5ListenerCore>(
                     toSeat, [allocator](Mqtt5ListenerCore *listener) { Crt::Delete(listener, allocator); });
-                shared_listener->m_selfReference = toSeat->shared_from_this();
+                shared_listener->m_selfReference = shared_listener;
                 return shared_listener;
             }
 
@@ -72,47 +197,20 @@ namespace Aws
 
                 if (client == nullptr)
                 {
-                    AWS_LOGF_DEBUG(AWS_LS_MQTT5_GENERAL, "Failed to create Mqtt5ListenerCore. Invalid Client.");
+                    AWS_LOGF_ERROR(AWS_LS_MQTT5_GENERAL, "Failed to create Mqtt5ListenerCore. Invalid Client.");
                     aws_raise_error(AWS_ERROR_INVALID_ARGUMENT);
                     return;
                 }
 
                 m_mqtt5Client = client->shared_from_this();
-                if (options.onConnectionFailure)
-                {
-                    this->onConnectionFailure = options.onConnectionFailure;
-                }
-
-                if (options.onConnectionSuccess)
-                {
-                    this->onConnectionSuccess = options.onConnectionSuccess;
-                }
-
-                if (options.onDisconnection)
-                {
-                    this->onDisconnection = options.onDisconnection;
-                }
-
-                if (options.onListenerPublishReceived)
-                {
-                    this->onListenerPublishReceived = options.onListenerPublishReceived;
-                }
-
-                if (options.onStopped)
-                {
-                    this->onStopped = options.onStopped;
-                }
-
-                if (options.onAttemptingConnect)
-                {
-                    this->onAttemptingConnect = options.onAttemptingConnect;
-                }
-
-                if (options.onListenerTermination)
-                {
-                    this->onListenerTermination = options.onListenerTermination;
-                    this->termination_userdata = options.termination_userdata;
-                }
+                this->onConnectionFailure = options.onConnectionFailure;
+                this->onConnectionSuccess = options.onConnectionSuccess;
+                this->onDisconnection = options.onDisconnection;
+                this->onListenerPublishReceived = options.onListenerPublishReceived;
+                this->onStopped = options.onStopped;
+                this->onAttemptingConnect = options.onAttemptingConnect;
+                this->onListenerTermination = options.onListenerTermination;
+                this->termination_userdata = options.termination_userdata;
 
                 aws_mqtt5_listener_config listener_config;
                 listener_config.listener_callbacks.lifecycle_event_handler =
@@ -136,22 +234,24 @@ namespace Aws
                 Mqtt5ListenerCore *listener = reinterpret_cast<Mqtt5ListenerCore *>(event->user_data);
                 if (listener == nullptr)
                 {
-                    AWS_LOGF_INFO(AWS_LS_MQTT5_GENERAL, "Lifecycle event: error retrieving callback userdata. ");
+                    AWS_LOGF_ERROR(
+                        AWS_LS_MQTT5_GENERAL, "Listener Lifecycle Event: error retrieving callback userdata. ");
                     return;
                 }
 
                 std::lock_guard<std::recursive_mutex> lock(listener->m_callback_lock);
                 if (listener->m_callbackFlag != Mqtt5ListenerCore::CallbackFlag::INVOKE)
                 {
-                    AWS_LOGF_INFO(
-                        AWS_LS_MQTT5_GENERAL, "Lifecycle event: mqtt5 listener is not valid, revoke the callbacks.");
+                    AWS_LOGF_DEBUG(
+                        AWS_LS_MQTT5_GENERAL,
+                        "Listener Lifecycle Event: mqtt5 listener is not valid, revoke the callbacks.");
                     return;
                 }
 
                 switch (event->event_type)
                 {
                     case AWS_MQTT5_CLET_STOPPED:
-                        AWS_LOGF_INFO(AWS_LS_MQTT5_GENERAL, "Lifecycle event: listener Stopped!");
+                        AWS_LOGF_DEBUG(AWS_LS_MQTT5_GENERAL, "Listener Lifecycle Event: listener Stopped!");
                         if (listener->onStopped != nullptr)
                         {
                             OnStoppedEventData eventData;
@@ -160,7 +260,7 @@ namespace Aws
                         break;
 
                     case AWS_MQTT5_CLET_ATTEMPTING_CONNECT:
-                        AWS_LOGF_INFO(AWS_LS_MQTT5_GENERAL, "Lifecycle event: Attempting Connect!");
+                        AWS_LOGF_DEBUG(AWS_LS_MQTT5_GENERAL, "Listener Lifecycle Event: Attempting Connect!");
                         if (listener->onAttemptingConnect != nullptr)
                         {
                             OnAttemptingConnectEventData eventData;
@@ -169,10 +269,9 @@ namespace Aws
                         break;
 
                     case AWS_MQTT5_CLET_CONNECTION_FAILURE:
-                        AWS_LOGF_INFO(AWS_LS_MQTT5_GENERAL, "Lifecycle event: Connection Failure!");
-                        AWS_LOGF_INFO(
+                        AWS_LOGF_DEBUG(
                             AWS_LS_MQTT5_GENERAL,
-                            "  Error Code: %d(%s)",
+                            "Listener Lifecycle Event: Connection Failure with error code: %d(%s)!",
                             event->error_code,
                             aws_error_debug_str(event->error_code));
                         if (listener->onConnectionFailure != nullptr)
@@ -191,7 +290,7 @@ namespace Aws
                         break;
 
                     case AWS_MQTT5_CLET_CONNECTION_SUCCESS:
-                        AWS_LOGF_INFO(AWS_LS_MQTT5_GENERAL, "Lifecycle event: Connection Success!");
+                        AWS_LOGF_DEBUG(AWS_LS_MQTT5_GENERAL, "Listener Lifecycle Event: Connection Success!");
                         if (listener->onConnectionSuccess != nullptr)
                         {
                             OnConnectionSuccessEventData eventData;
@@ -216,9 +315,9 @@ namespace Aws
                         break;
 
                     case AWS_MQTT5_CLET_DISCONNECTION:
-                        AWS_LOGF_INFO(
+                        AWS_LOGF_DEBUG(
                             AWS_LS_MQTT5_GENERAL,
-                            "  Error Code: %d(%s)",
+                            "Listener Lifecycle Event: Disconnect with error code: %d(%s)",
                             event->error_code,
                             aws_error_debug_str(event->error_code));
                         if (listener->onDisconnection != nullptr)
@@ -242,19 +341,27 @@ namespace Aws
                 const aws_mqtt5_packet_publish_view *publish,
                 void *user_data)
             {
-                AWS_LOGF_INFO(AWS_LS_MQTT5_GENERAL, "on listener publish received callback");
+                AWS_LOGF_DEBUG(AWS_LS_MQTT5_GENERAL, "on listener publish received callback");
                 Mqtt5ListenerCore *listener = reinterpret_cast<Mqtt5ListenerCore *>(user_data);
                 if (listener == nullptr)
                 {
-                    AWS_LOGF_INFO(
+                    AWS_LOGF_ERROR(
                         AWS_LS_MQTT5_GENERAL, "Listener Publish Received Event: Error retrieving callback userdata. ");
+                    return false;
+                }
+
+                if (listener->onListenerPublishReceived == nullptr)
+                {
+                    AWS_LOGF_DEBUG(
+                        AWS_LS_MQTT5_GENERAL,
+                        "Listener Publish Received Event: The publish received callback is not set.");
                     return false;
                 }
 
                 std::lock_guard<std::recursive_mutex> lock(listener->m_callback_lock);
                 if (listener->m_callbackFlag != Mqtt5ListenerCore::CallbackFlag::INVOKE)
                 {
-                    AWS_LOGF_INFO(
+                    AWS_LOGF_DEBUG(
                         AWS_LS_MQTT5_GENERAL,
                         "Listener Publish Received Event: Mqtt5 client is not valid, revoke the callbacks.");
                     return false;
@@ -274,13 +381,7 @@ namespace Aws
                     return false;
                 }
 
-                /* If the listener publish received is overwritten, then call the overwritten function. */
-                if (listener->onListenerPublishReceived != nullptr)
-                {
-                    return listener->onListenerPublishReceived(eventData);
-                }
-
-                return false;
+                return listener->onListenerPublishReceived(eventData);
             }
 
             void Mqtt5ListenerCore::s_listenerTerminationCompletion(void *complete_ctx)
@@ -296,14 +397,14 @@ namespace Aws
                 listener->m_selfReference.reset();
             }
 
-            std::shared_ptr<Mqtt5Listener> Mqtt5Listener::NewMqtt5Listener(
+            ScopedResource<Mqtt5Listener> Mqtt5Listener::NewMqtt5Listener(
                 const Mqtt5ListenerOptions &options,
                 const std::shared_ptr<Mqtt5Client> &client,
                 Allocator *allocator) noexcept
             {
                 if (client == nullptr)
                 {
-                    AWS_LOGF_DEBUG(AWS_LS_MQTT5_GENERAL, "Failed to create Mqtt5Listener. Invalid Client.");
+                    AWS_LOGF_ERROR(AWS_LS_MQTT5_GENERAL, "Failed to create Mqtt5Listener. Invalid Client.");
                     aws_raise_error(AWS_ERROR_INVALID_ARGUMENT);
                     return nullptr;
                 }
@@ -324,10 +425,18 @@ namespace Aws
                     Crt::Delete(toSeat, allocator);
                     return nullptr;
                 }
+                // //Custom Resource deleter
+                // struct Deleter {
+                //     //Called by unique_ptr to destroy/free the Resource
+                //     void operator()(Mqtt5Listener* listener) {
+                //         Crt::Delete(listener, allocator);
+                //     }
+                // };
 
-                std::shared_ptr<Mqtt5Listener> shared_listener = std::shared_ptr<Mqtt5Listener>(
+                ScopedResource<Mqtt5Listener> uniq_listener(
                     toSeat, [allocator](Mqtt5Listener *listener) { Crt::Delete(listener, allocator); });
-                return shared_listener;
+
+                return uniq_listener;
             }
 
             Mqtt5Listener::Mqtt5Listener(
@@ -348,13 +457,10 @@ namespace Aws
             }
 
             Mqtt5ListenerOptions::Mqtt5ListenerOptions() noexcept
+                : onAttemptingConnect(nullptr), onConnectionFailure(nullptr), onConnectionSuccess(nullptr),
+                  onDisconnection(nullptr), onStopped(nullptr), onListenerPublishReceived(nullptr),
+                  onListenerTermination(nullptr), termination_userdata(nullptr)
             {
-                onAttemptingConnect = NULL;
-                onConnectionFailure = NULL;
-                onConnectionSuccess = NULL;
-                onDisconnection = NULL;
-                onStopped = NULL;
-                onListenerPublishReceived = NULL;
             }
 
             Mqtt5ListenerOptions &Mqtt5ListenerOptions::WithListenerConnectionSuccessCallback(
