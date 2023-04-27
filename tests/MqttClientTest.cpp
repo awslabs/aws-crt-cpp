b954b364c0bb1610d02aaed542aac100c924b7c7
Noah Beard <ncbeard@amazon.com>
2023-04-27 13:38:01 -0400

    Add PKCS12 test

--- tests/MqttClientTest.cpp
+++ tests/MqttClientTest.cpp
@@ -50,6 +50,11 @@ AWS_STATIC_STRING_FROM_LITERAL(
     s_mqtt5_test_envName_iot_sign_custom_auth_tokensignature,
     "AWS_TEST_MQTT5_IOT_CORE_SIGNING_AUTHORIZER_TOKEN_SIGNATURE");
 
+AWS_STATIC_STRING_FROM_LITERAL(s_mqtt311_test_envName_iot_pkcs12_key, "AWS_TEST_MQTT311_IOT_CORE_PKCS12_KEY");
+AWS_STATIC_STRING_FROM_LITERAL(
+    s_mqtt311_test_envName_iot_pkcs12_key_password,
+    "AWS_TEST_MQTT311_IOT_CORE_PKCS12_KEY_PASSWORD");
+
 #if !BYO_CRYPTO
 static int s_TestMqttClientResourceSafety(Aws::Crt::Allocator *allocator, void *ctx)
 {
@@ -349,3 +354,97 @@ static int s_TestIoTMqtt311ConnectWithSigningCustomAuth(Aws::Crt::Allocator *all
     return AWS_OP_SUCCESS;
 }
 AWS_TEST_CASE(IoTMqtt311ConnectWithSigningCustomAuth, s_TestIoTMqtt311ConnectWithSigningCustomAuth)
+
+/*
+ * PKCS12 connect for MQTT311
+ */
+static int s_TestIoTMqtt311ConnectWithPKCS12(Aws::Crt::Allocator *allocator, void *)
+{
+    struct aws_string *endpoint = NULL;
+    struct aws_string *pkcs12_key = NULL;
+    struct aws_string *pkcs12_password = NULL;
+
+    int error = aws_get_environment_value(allocator, s_mqtt5_test_envName_iot_hostname, &endpoint);
+    error |= aws_get_environment_value(allocator, s_mqtt311_test_envName_iot_pkcs12_key, &pkcs12_key);
+    error |= aws_get_environment_value(allocator, s_mqtt311_test_envName_iot_pkcs12_key_password, &pkcs12_password);
+
+    bool isEveryEnvVarSet = (endpoint && pkcs12_key && pkcs12_password);
+    if (isEveryEnvVarSet == true)
+    {
+        isEveryEnvVarSet =
+            (aws_string_is_valid(endpoint) && aws_string_is_valid(pkcs12_key) && aws_string_is_valid(pkcs12_password));
+    }
+    if (error != AWS_OP_SUCCESS || isEveryEnvVarSet == false)
+    {
+        printf("Environment Variables are not set for the test, skip the test");
+        aws_string_destroy(endpoint);
+        aws_string_destroy(pkcs12_key);
+        aws_string_destroy(pkcs12_password);
+        return AWS_OP_SKIP;
+    }
+
+    Aws::Crt::ApiHandle apiHandle(allocator);
+
+    Aws::IoT::Pkcs12Options pkcs12Options = {.pkcs12_file = aws_string_c_str(pkcs12_key),
+                                             .pkcs12_password = aws_string_c_str(pkcs12_password)}
+
+    Aws::Iot::MqttClient client;
+    auto clientConfigBuilder =
+        Aws::Iot::MqttClientConnectionConfigBuilder::MqttClientConnectionConfigBuilder(pkcs12Options, allocator);
+    clientConfigBuilder.WithEndpoint(aws_string_c_str(endpoint));
+    auto clientConfig = clientConfigBuilder.Build();
+    if (!clientConfig)
+    {
+        printf("Failed to create MQTT311 client from config");
+        ASSERT_TRUE(false);
+    }
+    auto connection = client.NewConnection(clientConfig);
+    if (!*connection)
+    {
+        printf("Failed to create MQTT311 connection from config");
+        ASSERT_TRUE(false);
+    }
+
+    std::promise<bool> connectionCompletedPromise;
+    std::promise<void> connectionClosedPromise;
+    auto onConnectionCompleted =
+        [&](Aws::Crt::Mqtt::MqttConnection &, int errorCode, Aws::Crt::Mqtt::ReturnCode returnCode, bool) {
+            (void)returnCode;
+            if (errorCode)
+            {
+                connectionCompletedPromise.set_value(false);
+            }
+            else
+            {
+                connectionCompletedPromise.set_value(true);
+            }
+        };
+    auto onDisconnect = [&](Aws::Crt::Mqtt::MqttConnection &) { connectionClosedPromise.set_value(); };
+    connection->OnConnectionCompleted = std::move(onConnectionCompleted);
+    connection->OnDisconnect = std::move(onDisconnect);
+
+    Aws::Crt::UUID Uuid;
+    Aws::Crt::String uuidStr = Uuid.ToString();
+
+    if (!connection->Connect(uuidStr.c_str(), true /*cleanSession*/, 5000 /*keepAliveTimeSecs*/))
+    {
+        printf("Failed to connect");
+        ASSERT_TRUE(false);
+    }
+    if (connectionCompletedPromise.get_future().get() == false)
+    {
+        printf("Connection failed");
+        ASSERT_TRUE(false);
+    }
+    if (connection->Disconnect())
+    {
+        connectionClosedPromise.get_future().wait();
+    }
+
+    aws_string_destroy(endpoint);
+    aws_string_destroy(pkcs12_key);
+    aws_string_destroy(pkcs12_password);
+    aws_string_destroy(empty_string);
+    return AWS_OP_SUCCESS;
+}
+AWS_TEST_CASE(IoTMqtt311ConnectWithPKCS12, s_TestIoTMqtt311ConnectWithPKCS12)
