2a52be15b63420840e5c484963cf77e6eb9ae40a
Noah Beard <ncbeard@amazon.com>
2023-05-09 17:12:23 -0400

    Use DUSE_OPENSSL=ON to run PKCS11 in CI, install the SSL development lib if needed

--- .builder/actions/build_crt.py
+++ .builder/actions/build_crt.py
@@ -1,4 +1,5 @@
 import Builder
+import sys
 
 
 class BuildCrt(Builder.Action):
@@ -23,4 +24,22 @@ class BuildCrt(Builder.Action):
 
             new_args.append(arg)
 
+        # If on Linux, we want to use the OS crypto library so we can run PKCS11 tests in CI
+        if (sys.platform.startswith("linux") or sys.platform.startswith("freebsd")):
+            did_install_openssl = True
+            print ("Trying to install a SSL development library (either libssl-dev or openssl-devel)")
+            # Make sure libssl-dev is installed
+            try:
+                Builder.InstallPackages(['libssl-dev']).run(env)
+                print ("Installed/Updated libssl-dev")
+            except:
+                try:
+                    Builder.InstallPackages(['openssl-devel']).run(env)
+                    print ("Installed/Updated openssl-devel")
+                except:
+                    print ("ERROR - could not install either libssl-devel or openssl-devel. Skipping setting '-DUSE_OPENSSL=ON'")
+                    did_install_openssl = False
+            if (did_install_openssl):
+                new_args.append("-DUSE_OPENSSL=ON")
         return new_args
+
