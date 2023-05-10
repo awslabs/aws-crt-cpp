568f8986a19380cc38fe658fd949d9421f4e8791
Noah Beard <ncbeard@amazon.com>
2023-05-10 13:27:07 -0400

    Did not fix the will thing after all

--- tests/Mqtt5ClientTest.cpp
+++ tests/Mqtt5ClientTest.cpp
@@ -30,45 +30,18 @@ static void s_setupConnectionLifeCycle(
     mqtt5Options.WithClientConnectionSuccessCallback(
         [&connectionPromise, clientName](const OnConnectionSuccessEventData &) {
             printf("[MQTT5]%s Connection Success.", clientName);
-            try
-            {
-                connectionPromise.set_value(true);
-            }
-            catch (...)
-            {
-                // Not ideal, but: this prevents the issue of the promise being set twice causing an exception
-                // which can happen on slow containers (I.E Codebuild, GitHub Actions)
-                printf("[MQTT5]%s Connection Success - but exception ocurred setting future", clientName);
-            }
+            connectionPromise.set_value(true);
         });
 
     mqtt5Options.WithClientConnectionFailureCallback(
         [&connectionPromise, clientName](const OnConnectionFailureEventData &eventData) {
             printf("[MQTT5]%s Connection failed with error : %s", clientName, aws_error_debug_str(eventData.errorCode));
-            try
-            {
-                connectionPromise.set_value(false);
-            }
-            catch (...)
-            {
-                // Not ideal, but: this prevents the issue of the promise being set twice causing an exception
-                // which can happen on slow containers (I.E Codebuild, GitHub Actions)
-                printf("[MQTT5]%s Connection failed - but exception ocurred setting future", clientName);
-            }
+            connectionPromise.set_value(false);
         });
 
     mqtt5Options.WithClientStoppedCallback([&stoppedPromise, clientName](const OnStoppedEventData &) {
         printf("[MQTT5]%s Stopped", clientName);
-        try
-        {
-            stoppedPromise.set_value();
-        }
-        catch (...)
-        {
-            // Not ideal, but: this prevents the issue of the promise being set twice causing an exception
-            // which can happen on slow containers (I.E Codebuild, GitHub Actions)
-            printf("[MQTT5]%s Stopped - but exception ocurred setting future", clientName);
-        }
+        stoppedPromise.set_value();
     });
 }
 
