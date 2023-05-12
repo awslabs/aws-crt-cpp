5679c9da1be69bb2daca82be40b9bbb5d0a678c2
Noah Beard <ncbeard@amazon.com>
2023-05-12 11:57:37 -0400

    Codebuild time. See if the environment tests are being skipped due to missing environment variables

--- tests/Mqtt5ClientCredentialsTest.cpp
+++ tests/Mqtt5ClientCredentialsTest.cpp
@@ -1018,9 +1018,10 @@ static int s_TestIoTMqtt5ConnectWSEnvironment(Aws::Crt::Allocator *allocator, vo
     if (error != AWS_OP_SUCCESS || isEveryEnvVarSet == false)
     {
         printf("Environment Variables are not set for the test, skip the test");
-        aws_string_destroy(endpoint);
-        aws_string_destroy(region);
-        return AWS_OP_SKIP;
+        // TEST - since this test seems to always be skipped, let's see if commenting this out helps
+        // aws_string_destroy(endpoint);
+        // aws_string_destroy(region);
+        // return AWS_OP_SKIP;
     }
 
     ApiHandle apiHandle(allocator);
--- tests/MqttClientCredentialsTest.cpp
+++ tests/MqttClientCredentialsTest.cpp
@@ -1199,9 +1199,10 @@ static int s_TestIoTMqtt311ConnectWSEnvironment(Aws::Crt::Allocator *allocator, 
     if (error != AWS_OP_SUCCESS || isEveryEnvVarSet == false)
     {
         printf("Environment Variables are not set for the test, skip the test");
-        aws_string_destroy(endpoint);
-        aws_string_destroy(region);
-        return AWS_OP_SKIP;
+        // TEST - since this test seems to always be skipped, let's see if commenting this out helps
+        // aws_string_destroy(endpoint);
+        // aws_string_destroy(region);
+        // return AWS_OP_SKIP;
     }
 
     Aws::Crt::ApiHandle apiHandle(allocator);
