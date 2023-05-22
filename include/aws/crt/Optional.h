553bb6f35f0d920b381be606489bf9794d91d9b0
Waqar Ahmed Khan <waahm7@gmail.com>
2023-05-22 09:59:04 -0700

    try removing macro from optional.h

--- include/aws/crt/Optional.h
+++ include/aws/crt/Optional.h
@@ -3,10 +3,8 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-#include <aws/common/macros.h>
 #include <utility>
 
-AWS_PUSH_SANE_WARNING_LEVEL
 namespace Aws
 {
     namespace Crt
@@ -203,4 +201,3 @@ namespace Aws
         };
     } // namespace Crt
 } // namespace Aws
-AWS_POP_SANE_WARNING_LEVEL
