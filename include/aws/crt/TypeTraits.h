45729ae2ef5991639d7c2978fd8927d705f0c70c
Igor Abdrakhimov <igorabd@amazon.com>
2025-03-27 09:30:03 -0700

    Add comment

--- include/aws/crt/TypeTraits.h
+++ include/aws/crt/TypeTraits.h
@@ -13,6 +13,8 @@ namespace Aws
         /**
          * A type trait for determining if the first template parameter is a template specialization of the second
          * template parameter. Based on p2098 (https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/p2098r1.pdf).
+         *
+         * @note Known limitations: does not support template classes with non-type template parameter, e.g. std::array.
          */
         template <typename T, template <typename...> class Primary> struct IsSpecializationOf : std::false_type
         {
