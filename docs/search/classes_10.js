a6e64175747f78106cb9b28d3dee034c7d43587b
GitHub Action <action@github.com>
2025-08-27 16:57:38 +0000

    update docs

---
+++ docs/search/classes_10.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['variant_0',['Variant',['../class_aws_1_1_crt_1_1_variant.html',1,'Aws::Crt']]],
+  ['variant_3c_20r_2c_20e_20_3e_1',['Variant&lt; R, E &gt;',['../class_aws_1_1_crt_1_1_variant.html',1,'Aws::Crt']]],
+  ['variantalternative_2',['VariantAlternative',['../class_aws_1_1_crt_1_1_variant_alternative.html',1,'Aws::Crt']]],
+  ['variantsize_3',['VariantSize',['../class_aws_1_1_crt_1_1_variant_size.html',1,'Aws::Crt']]],
+  ['version_4',['Version',['../struct_aws_1_1_crt_1_1_api_handle_1_1_version.html',1,'Aws::Crt::ApiHandle']]]
+];
---
+++ docs/search/classes_5.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['hash_0',['Hash',['../class_aws_1_1_crt_1_1_crypto_1_1_hash.html',1,'Aws::Crt::Crypto']]],
+  ['hastype_1',['HasType',['../struct_aws_1_1_crt_1_1_variant_detail_1_1_checker_1_1_has_type.html',1,'Aws::Crt::VariantDetail::Checker']]],
+  ['hmac_2',['HMAC',['../class_aws_1_1_crt_1_1_crypto_1_1_h_m_a_c.html',1,'Aws::Crt::Crypto']]],
+  ['hostresolver_3',['HostResolver',['../class_aws_1_1_crt_1_1_io_1_1_host_resolver.html',1,'Aws::Crt::Io']]],
+  ['httpclientconnection_4',['HttpClientConnection',['../class_aws_1_1_crt_1_1_http_1_1_http_client_connection.html',1,'Aws::Crt::Http']]],
+  ['httpclientconnectionmanager_5',['HttpClientConnectionManager',['../class_aws_1_1_crt_1_1_http_1_1_http_client_connection_manager.html',1,'Aws::Crt::Http']]],
+  ['httpclientconnectionmanageroptions_6',['HttpClientConnectionManagerOptions',['../class_aws_1_1_crt_1_1_http_1_1_http_client_connection_manager_options.html',1,'Aws::Crt::Http']]],
+  ['httpclientconnectionoptions_7',['HttpClientConnectionOptions',['../class_aws_1_1_crt_1_1_http_1_1_http_client_connection_options.html',1,'Aws::Crt::Http']]],
+  ['httpclientconnectionproxyoptions_8',['HttpClientConnectionProxyOptions',['../class_aws_1_1_crt_1_1_http_1_1_http_client_connection_proxy_options.html',1,'Aws::Crt::Http']]],
+  ['httpclientstream_9',['HttpClientStream',['../class_aws_1_1_crt_1_1_http_1_1_http_client_stream.html',1,'Aws::Crt::Http']]],
+  ['httpmessage_10',['HttpMessage',['../class_aws_1_1_crt_1_1_http_1_1_http_message.html',1,'Aws::Crt::Http']]],
+  ['httpproxystrategy_11',['HttpProxyStrategy',['../class_aws_1_1_crt_1_1_http_1_1_http_proxy_strategy.html',1,'Aws::Crt::Http']]],
+  ['httpproxystrategyadaptiveconfig_12',['HttpProxyStrategyAdaptiveConfig',['../struct_aws_1_1_crt_1_1_http_1_1_http_proxy_strategy_adaptive_config.html',1,'Aws::Crt::Http']]],
+  ['httpproxystrategybasicauthconfig_13',['HttpProxyStrategyBasicAuthConfig',['../struct_aws_1_1_crt_1_1_http_1_1_http_proxy_strategy_basic_auth_config.html',1,'Aws::Crt::Http']]],
+  ['httprequest_14',['HttpRequest',['../class_aws_1_1_crt_1_1_http_1_1_http_request.html',1,'Aws::Crt::Http']]],
+  ['httprequestoptions_15',['HttpRequestOptions',['../struct_aws_1_1_crt_1_1_http_1_1_http_request_options.html',1,'Aws::Crt::Http']]],
+  ['httpresponse_16',['HttpResponse',['../class_aws_1_1_crt_1_1_http_1_1_http_response.html',1,'Aws::Crt::Http']]],
+  ['httpsignercallbackdata_17',['HttpSignerCallbackData',['../struct_aws_1_1_crt_1_1_auth_1_1_http_signer_callback_data.html',1,'Aws::Crt::Auth']]],
+  ['httpstream_18',['HttpStream',['../class_aws_1_1_crt_1_1_http_1_1_http_stream.html',1,'Aws::Crt::Http']]]
+];
