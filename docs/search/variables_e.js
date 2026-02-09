0e49ff9115e6a7acb810d5f41fc4e9ff2ae20222
GitHub Action <action@github.com>
2026-02-09 21:31:52 +0000

    update docs

---
+++ docs/search/variables_e.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['ramdiskid_0',['ramdiskid',['../struct_aws_1_1_crt_1_1_imds_1_1_instance_info_view.html#a0e42cd6acbe53402c95d380a2e3d6148',1,'Aws::Crt::Imds::InstanceInfoView::ramdiskId'],['../struct_aws_1_1_crt_1_1_imds_1_1_instance_info.html#a8b06121d9b115d3da32c1c353ae67afa',1,'Aws::Crt::Imds::InstanceInfo::ramdiskId']]],
+  ['region_1',['region',['../struct_aws_1_1_crt_1_1_auth_1_1_credentials_provider_s_t_s_web_identity_config.html#a535b473cf72aa98febd1afb0346ff05c',1,'Aws::Crt::Auth::CredentialsProviderSTSWebIdentityConfig::Region'],['../struct_aws_1_1_crt_1_1_imds_1_1_instance_info_view.html#a4ffb39bb7e75411e45aca3e7229ed857',1,'Aws::Crt::Imds::InstanceInfoView::region'],['../struct_aws_1_1_crt_1_1_imds_1_1_instance_info.html#a15f3fb34ccbe7369100b58ba5d013266',1,'Aws::Crt::Imds::InstanceInfo::region']]],
+  ['request_2',['request',['../struct_aws_1_1_crt_1_1_http_1_1_http_request_options.html#a7ced6127e20305b55db702f1cd143ad0',1,'Aws::Crt::Http::HttpRequestOptions::request'],['../struct_aws_1_1_crt_1_1_auth_1_1_http_signer_callback_data.html#a2b30541a72b3eac86b4af60a4ab27a61',1,'Aws::Crt::Auth::HttpSignerCallbackData::Request']]],
+  ['returncode_3',['returnCode',['../struct_aws_1_1_crt_1_1_mqtt_1_1_on_connection_success_data.html#a8dd68c8feac6b04c367dc0129571271c',1,'Aws::Crt::Mqtt::OnConnectionSuccessData']]],
+  ['rolealias_4',['RoleAlias',['../struct_aws_1_1_crt_1_1_auth_1_1_credentials_provider_x509_config.html#a6ab5fc641d37391e7cc808c61efc6e89',1,'Aws::Crt::Auth::CredentialsProviderX509Config']]],
+  ['rolearn_5',['rolearn',['../struct_aws_1_1_crt_1_1_auth_1_1_credentials_provider_s_t_s_config.html#afab7b973e14a534812feaab17a2fa002',1,'Aws::Crt::Auth::CredentialsProviderSTSConfig::RoleArn'],['../struct_aws_1_1_crt_1_1_auth_1_1_credentials_provider_s_t_s_web_identity_config.html#a82b41d7c2c376706ee6a2734b2b91949',1,'Aws::Crt::Auth::CredentialsProviderSTSWebIdentityConfig::RoleArn']]]
+];
