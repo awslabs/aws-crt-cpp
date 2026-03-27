c72c35c40b5ace7e765e1da2e556bd89dbb89d11
sbiscigl <sbiscigl@amazon.com>
2026-03-27 14:13:18 -0400

    bind out proxy settings for profile and web identity creds provider

--- source/http/HttpConnection.cpp
+++ source/http/HttpConnection.cpp
@@ -398,6 +398,18 @@ namespace Aws
                 }
             }
 
+            void ProxyEnvVarOptions::InitializeRawProxyOptions(struct proxy_env_var_settings &rawOptions) const
+            {
+                AWS_ZERO_STRUCT(rawOptions);
+                rawOptions.env_var_type = (enum aws_http_proxy_env_var_type)proxyEnvVarType;
+                rawOptions.connection_type = (enum aws_http_proxy_connection_type)connectionType;
+
+                if (TlsOptions.has_value())
+                {
+                    rawOptions.tls_options = TlsOptions->GetUnderlyingHandle();
+                }
+            }
+
             HttpClientConnectionOptions::HttpClientConnectionOptions()
                 : Bootstrap(nullptr), InitialWindowSize(SIZE_MAX), OnConnectionSetupCallback(),
                   OnConnectionShutdownCallback(), HostName(), Port(0), SocketOptions(), TlsOptions(), ProxyOptions(),
